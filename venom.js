33ad50bec36f3e2828253e65f0cc358f92a19f11
Keya <121007392+Cauazito1212@users.noreply.github.com>
2023-04-01 23:34:42 -0400

    Add files via upload

---
+++ venom.js
@@ -0,0 +1,18 @@
+/*            // leia antes //
+ Meu canal: https://youtube.com/@VenomModss
+ 
+ Meu canal secundário: https://youtube.com/@VenomMods2
+ 
+ Meu Whatsapp: https://wa.me/559784388524
+ 
+ Meu grupo1: https://chat.whatsapp.com/F0htRhluNlX8DZyBecONv0
+ 
+ Meu grupo2: https://chat.whatsapp.com/Eig4z1Oar9C9Yl58ro7T3n
+ 
+ Meu github: https://github.com/Venom-Ytb
+ 
+ se for resposta minha base deixe os créditos 
+ e se for fazer seu bot pfv deixa pelo menos isso 
+ tudo que está aki foi feito pra judar vocês 
+ É mereço pelos menos os créditos...
+ Qualquer base parecida ou semelhante a essa e kibado. */
